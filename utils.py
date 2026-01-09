74674d7330bec974ae0f4482e561cfd4b0d685e1
zaranaragomzi-a11y <zaranaragomzi@gmail.com>
2026-01-09 15:32:02 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+import pdfplumber
+
+def extract_text_from_pdf(file):
+    text = ""
+    with pdfplumber.open(file) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text() or ""
+    return text.strip()
